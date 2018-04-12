8fdf43d9af301aed5072af5983c1feda6a661ab6
Matt Rota <mrota@stevens.edu>
2018-04-12 16:17:44 -0400

    updated VMsimulator

--- VMsimulator.cpp
+++ VMsimulator.cpp
@@ -3,6 +3,17 @@
 #include <sstream>		// std::istringstream
 #include <cstdlib>		// std::exit
 #include <string>		// std::string
+#include <vector>		// std::vector
+#include <array>		// std::array
+
+unsigned long pageid;
+unsigned long age = 0;
+
+struct page_t {
+	unsigned long page_number;
+	int valid_bit;
+	unsigned long last_accessed;
+}
 
 int main(int argc, char* argv[]) {
 	if (argc != 6) {
@@ -23,6 +34,8 @@ int main(int argc, char* argv[]) {
 	const int PAGE_SIZE = x;
 	const char* RALG	= argv[4];
 	const char* PREPAGE = argv[5];
+
+	const int MEMORY_SIZE = 512;
 	
 /*	
 	We need to run a simulation for a virtual memry management system
@@ -36,11 +49,22 @@ int main(int argc, char* argv[]) {
 	// Let's read in the files we pass from the command line with ifstream
 	// PLIST: 	(pID, Total#MemoryLocation)
 	// PTRACE: 	(pId, ReferencedMemoryLocation)
+
+/*	
+	Data Structure Strategy
+	Hierarchy
+	Processes 			- List of page tables
+		-> Page Tables 	- List of pages
+			-> page 	- Page struct
+*/
+	std::vector<string> lines ();
+	std::vector<page_t[]> processes ();
+	std::vector<page_t> temp ();
+
 	std::ifstream ifs_plist (PLIST, std::ifstream::in);
 	std::ifstream ifs_ptrace (PTRACE, std::ifstream::in);
 	std::string pid, total_locations;
-	std::size_t pos;
-
+	std::size_t delimiter;									// Our delimiter here is the space
 	if (!ifs_plist.is_open()) {
 		std::cerr << "plist.txt: An error occurred while reading the file." << std::endl;
 		std::exit(EXIT_FAILURE);
@@ -48,13 +72,32 @@ int main(int argc, char* argv[]) {
 		std::cerr << "ptrace.txt: An error occurred while reading the file." << std::endl;
 		std::exit(EXIT_FAILURE);
 	} else {
+		
 		for(std::string line; std::getline(ifs_plist, line); ) {
-			pid = line.substr(0, 1); 			// PID
-			total_locations = line.substr(2); 	// Total number of memory locations
-			std::cout << "The pid is: " << pid
-			<< " and the total number of memory locations needed is: " 
-			<< total_locations << std::endl;
+			lines.push_back(line);
 		}
+
+		unsigned long proc_num = lines.size();
+		for(std::string line: lines) {
+			delimiter = line.find(" ");
+			total_locations = line.substr(delimiter+1);
+			for(unsigned long pages = 0; pages < )
+		}
+
+
+			// Create the pages and page table for the processes here.
+			for (unsgined long i = 0; i < ; i++) {
+				page_t page = {pageid++, 0, age};
+				temp.push_back(page);
+			}
+			page_t[] page_table = &temp[0];
+			processes.push_back(page_table);
+		}
+		
+		std::cout 
+		<< "Number of page tables: " << processes.size()
+		<< "Number of pages in page table 0: " << (sizeof(processes[0])/sizeof(*processes[0]))
+		<< std::endl;
 	}
 
 
---
+++ make.sh
@@ -0,0 +1,2 @@
+g++ -g VMsimulator.cpp -o VMsimulator
+gdb --args VMsimulator plist.txt ptrace.txt 2 FIFO +
